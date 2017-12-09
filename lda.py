e24059bde32196b896d868c4e56fa11677ca4efb
mparker3 <bigpark22@gmail.com>
2017-12-09 14:10:29 -0500

    changes and improvements for final copy

--- lda.py
+++ lda.py
@@ -3,6 +3,8 @@
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from nltk import pos_tag
 import re
 import string
 from collections import defaultdict
@@ -10,7 +12,8 @@ from gensim import corpora
 import gensim
 import os
 import sys
-def parse(path):
+import numpy as np
+def parse(path, doc):
     #loadCleanCorpus("35Corpora/CANON.txt")
     #too lengthy for testing
     
@@ -22,22 +25,33 @@ def parse(path):
             cleanText = loadCleanNovel(path + filename)
             corpus.append(cleanText)
         counter += 1
-    topicModel(corpus)
-
-def topicModel(corpus):
     
-    #split book into documents for testing
+    cleanDoc = loadCleanNovel(doc)
+    preppedDoc = prepDoc(cleanDoc)
+    corpusModel = topicModel(corpus, cleanDoc)
+    
+def prepDoc(doc):
+    chunks = []
+    for i in range(0, len(doc), 30):
+        chunks.append(doc[i:i+30])
+    return chunks
+
+
+def topicModel(corpus, query_text):
+    
     newcorpus = corpus
-    
     dictionary = corpora.Dictionary(newcorpus)
+    corpus = [dictionary.doc2bow(doc) for doc in corpus]
+    lda = gensim.models.ldamulticore.LdaMulticore
+    ldamodel = lda(corpus=corpus, num_topics=3, id2word = dictionary, workers=2, passes=50) 
     
-    doc_term_matrix = [dictionary.doc2bow(doc) for doc in newcorpus]
-
-    lda = gensim.models.ldamodel.LdaModel
-
-    ldamodel = lda(doc_term_matrix, num_topics=3, id2word = dictionary, passes=50)
-
-    print(ldamodel.print_topics(num_topics=3, num_words=3))
+    query_text_bow = dictionary.doc2bow(query_text)
+    query_text_lda = ldamodel[query_text_bow]
+    sametopic = 0
+    topics = 0
+    print(ldamodel.print_topic(query_text_lda[0][0], topn=10))
+    
+    return ldamodel
 
 def loadCleanNovel(filename):
     
@@ -55,19 +69,25 @@ def loadCleanNovel(filename):
             tokenizedCorpusNoPunctuation.append(newToken)
     noStopwords = []
     stopwordsSet = set(stopwords.words('english'))
-    print(stopwordsSet)
     for word in tokenizedCorpusNoPunctuation:
         if word.lower() not in stopwordsSet and word.lower() not in specialchars:
             noStopwords.append(word)
-        else:
-            print(word)
     frequency = defaultdict(int)
     for token in noStopwords:
         frequency[token] += 1
     clean = [token for token in noStopwords if frequency [token] > 1]
 #    print(clean)
-    return clean
+    pos = pos_tag(clean)
+    nouns = []
+    for pair in pos:
+        if pair[1] in ["NN", "ADJ"] :
+            nouns.append(pair[0])
+    lesscommon = []
+    fdist = FreqDist(word for word in nouns)
+    mostCommon = fdist.most_common(25)
+    lesscommon = [word for word in nouns if word not in mostCommon]
+    return lesscommon
 
 if __name__ == "__main__":
     print("done importing")
-    parse(sys.argv[1])
+    parse(sys.argv[1], sys.argv[2])
